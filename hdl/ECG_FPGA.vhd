30be013b0c1afc76498c78fad3552db82d0f4b27
Fer Clerkx <f.clerkx@student.fontys.nl>
2022-12-06 15:53:48 +0100

    Init commit

---
+++ hdl/ECG_FPGA.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ECG_FPGA is
+	port (
+	);
+end ECG_FPGA;
+
+architecture RTL of ECG_FPGA is
+
+begin
+
+end architecture RTL;
