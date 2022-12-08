83d46a46c8baef718909532046cb13ccd19d6064
Fer Clerkx <f.clerkx@student.fontys.nl>
2022-12-08 01:06:31 +0100

    Improved architecture of VGA driver

--- hdl/VGA_driver.vhd
+++ hdl/VGA_driver.vhd
@@ -18,54 +18,54 @@ end VGA_driver;
 
 architecture RTL of VGA_driver is
 
-	-- constant c_MAX :
-	-- type t_QUEUE is array(0 to 100) of t_WR_REQ;
-	-- signal r_QUEUE : t_QUEUE;
+	constant c_SECTIONS : integer := 1;
+	signal r_Section : integer range 0 to c_SECTIONS;
+
 	signal r_Count : integer := 0;
+	type t_MAX is array(0 to c_SECTIONS-1) of integer;
+	constant c_MAX : t_MAX := (
+		0 => 20
+	);
 
 begin
 
-	process(i_clk, i_rst)
+	CONTROLLER : process(i_clk, i_rst)
 	begin
 		if i_rst = '1' then
 			o_valid <= '0';
+			r_Section <= 0;
 			r_Count <= 0;
 		elsif rising_edge(i_clk) then
-			if r_Count < 20 then
-				o_buf_sel <= '1';
+			if i_ready = '1' and r_Section < c_SECTIONS then
 				o_valid <= '1';
-				o_wr_req.sel <= 43;
-				o_wr_req.scale <= 1;
-				o_wr_req.h_addr <= r_Count*16;
-				o_wr_req.v_addr <= 40;
-				if i_ready = '1' then
-					r_Count <= r_Count + 1;
-				end if;
-			elsif r_Count < 25 then
 				o_buf_sel <= '1';
-				o_valid <= '1';
-				o_wr_req.sel <= 43;
-				o_wr_req.scale <= 1;
-				o_wr_req.h_addr <= 20*16;
-				o_wr_req.v_addr <= (r_Count-20)*20;
-				if i_ready = '1' then
+				if r_Count < c_MAX(r_Section) then
 					r_Count <= r_Count + 1;
-				end if;
-			elsif r_Count < 25 then
-				o_buf_sel <= '1';
-				o_valid <= '1';
-				o_wr_req.sel <= 45;
-				o_wr_req.scale <= 1;
-				o_wr_req.h_addr <= 21*16;
-				o_wr_req.v_addr <= (r_Count-25)*20;
-				if i_ready = '1' then
-					r_Count <= r_Count + 1;
+				else
+					r_Count <= 0;
+					r_Section <= r_Section + 1;
 				end if;
 			else
 				o_valid <= '0';
 				o_buf_sel <= '0';
 			end if;
 		end if;
+	end process;
+
+	DATAPATH : process(r_Section, r_Count)
+	begin
+		case r_Section  is
+		when 0 =>
+			o_wr_req.sel <= 43;
+			o_wr_req.scale <= 1;
+			o_wr_req.h_addr <= r_Count*16;
+			o_wr_req.v_addr <= 10;
+		when 1 =>
+			o_wr_req.sel <= 0;
+			o_wr_req.scale <= 0;
+			o_wr_req.h_addr <= 0;
+			o_wr_req.v_addr <= 0;
+		end case;
 	end process;
 
 end architecture RTL;
