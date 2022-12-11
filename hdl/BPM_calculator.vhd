08875a84067fd29ec30e695df14df00e1c557347
Fer Clerkx <f.clerkx@student.fontys.nl>
2022-12-12 00:29:38 +0100

    Changed BPM calculator to work in 50MHz clock domain

--- hdl/BPM_calculator.vhd
+++ hdl/BPM_calculator.vhd
@@ -4,13 +4,14 @@ use ieee.numeric_std.all;
 
 entity BPM_calculator is
 	generic (
-		g_CLK_PERIOD	: integer := 6667;
-		g_THRESHOLD		: unsigned(11 downto 0) := "100000000000"
+		g_DATA_RATE	: integer := 150;
+		g_THRESHOLD	: unsigned(11 downto 0) := X"800"
 	);
 	port (
 		i_clk	: in std_logic;
 		i_rst	: in std_logic;
 		i_data	: in std_logic_vector(11 downto 0);
+		i_valid	: in std_logic;
 		o_bpm	: out integer range 0 to 999
 	);
 end BPM_calculator;
@@ -30,27 +31,29 @@ begin
 			r_Count <= 0;
 			r_State <= 0;
 		elsif rising_edge(i_clk) then
-			v_Data := unsigned(i_data);
-			r_Count <= r_Count + 1;
-			case r_State is
-			when 0 =>
-				if v_Data > g_THRESHOLD then
-					r_State <= 1;
-					r_Max <= v_Data;
-				end if;
-			when 1 =>
-				if v_Data > r_Max then
-					r_Max <= v_Data;
-				else
-					r_State <= 2;
-					o_bpm <= 60 / (r_Count*g_CLK_PERIOD/1E6);
-					r_Count <= 0;
-				end if;
-			when 2 =>
-				if v_Data < g_THRESHOLD then
-					r_State <= 0;
-				end if;
-			end case;
+			if i_valid = '1' then
+				v_Data := unsigned(i_data);
+				r_Count <= r_Count + 1;
+				case r_State is
+				when 0 =>
+					if v_Data > g_THRESHOLD then
+						r_State <= 1;
+						r_Max <= v_Data;
+					end if;
+				when 1 =>
+					if v_Data > r_Max then
+						r_Max <= v_Data;
+					else
+						r_State <= 2;
+						o_bpm <= 60 / (r_Count*g_DATA_RATE);
+						r_Count <= 0;
+					end if;
+				when 2 =>
+					if v_Data < g_THRESHOLD then
+						r_State <= 0;
+					end if;
+				end case;
+			end if;
 		end if;
 	end process;
 
